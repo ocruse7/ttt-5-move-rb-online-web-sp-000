cc610768d7dcd60989a8224afe2bccfa8f5aae7e
Owen Cruse <ocruse7@gmail.com>
2020-11-02 19:40:25 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,8 +6,9 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(i)
-  choice = (i.to_i) - 1
+def input_to_index(input)
+  choice = (input.to_i) - 1
+  return choice
 end
 
 def move(board, choice, player_character)
